d92b5dcd6f6f7beaad6240267ebe46ed85651fd7
Iewnfod <shangzhiqiuyue@outlook.com>
2023-06-12 11:13:32 +0800

    Bug fix

--- src/ui/network.rs
+++ src/ui/network.rs
@@ -104,12 +104,7 @@ async fn download(url: &String, save_path: &String) -> bool {
     std::fs::create_dir_all(prefix).unwrap();
 
     // 发送请求，获取文件
-    let response = match reqwest::get(url).await {
-        Ok(r) => r,
-        Err(_) => {
-            return false;
-        }
-    };
+    let response = reqwest::get(url).await.unwrap();
 
     println!("{} : {}", save_path, response.status());
 
