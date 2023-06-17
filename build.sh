5127658b888f15b8ad697d7eeaf3ed63026f9ad7
Iewnfod <shangzhiqiuyue@outlook.com>
2023-06-17 19:00:40 +0800

    Update build script on mac

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-# 删除之前的构建文件
-rm -R target/release
 # 构建
 cargo build --release
 # 去除调试信息
 strip -o target/release/get_paper_rs-stripped target/release/get_paper_rs
+# 重命名
+mv target/release/get_paper_rs target/release/get_paper_rs-darwin
+mv target/release/get_paper_rs-stripped target/release/get_paper_rs-darwin-stripped
