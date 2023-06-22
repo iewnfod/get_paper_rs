760dbabe232a00d7ba43207838c00c0e61180497
Iewnfod <shangzhiqiuyue@outlook.com>
2023-06-22 12:51:02 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
+remove-item 'target\release\get_paper_rs.exe'
+remove-item 'target\release\get_paper_rs-windows.exe'
 cargo build --release
 rename-item 'target\release\get_paper_rs.exe' -newname 'get_paper_rs-windows.exe'
